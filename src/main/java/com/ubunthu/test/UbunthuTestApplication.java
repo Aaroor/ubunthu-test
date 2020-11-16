79adf6d64cb2fe470250f72057bb6ad8b3c3a3b0
Aarooran kanthasamy <aaroor121819@gmail.com>
2020-11-16 16:27:47 +0530

    added

---
+++ src/main/java/com/ubunthu/test/UbunthuTestApplication.java
@@ -0,0 +1,13 @@
+package com.ubunthu.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbunthuTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UbunthuTestApplication.class, args);
+	}
+
+}
